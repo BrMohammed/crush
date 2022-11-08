feb1c3be8a08ebe4a2cd1f690110d14606c60f4a
mohammed <togachivon@gmail.com>
2022-11-08 23:59:33 +0100

    end_prototype

--- Assets/scripts/Criation_new_map.cs
+++ Assets/scripts/Criation_new_map.cs
@@ -11,21 +11,22 @@ public class Criation_new_map : MonoBehaviour
     [SerializeField]
     private GameObject cube;
     GameObject c;
+
     private void Start()
     {
         Color[] pixels = ImageRaw.GetPixels();
 
 
-        int wordZ = ImageRaw.height;
+        int wordY = ImageRaw.height;
         int wordX = ImageRaw.width;
 
         Vector3[] Spawnositions = new Vector3[pixels.Length];
-        Vector3 startingswapposition = new Vector3(-Mathf.Round(wordX / 2), 0, -Mathf.Round(wordZ / 2));
+        Vector3 startingswapposition = new Vector3(-Mathf.Round(wordX / 2), -Mathf.Round(wordY / 2), 0);
         Vector3 currentSpawnposetion = startingswapposition;
 
         int counter = 0;
 
-        for (int z = 0; z < wordZ; z++)
+        for (int y = 0; y < wordY; y++)
         {
             for (int x = 0; x < wordX; x++)
             {
@@ -34,7 +35,7 @@ public class Criation_new_map : MonoBehaviour
                 currentSpawnposetion.x++;
             }
             currentSpawnposetion.x = startingswapposition.x;
-            currentSpawnposetion.z++;
+            currentSpawnposetion.y++;
         }
         counter = 0;
         foreach (Vector3 pos in Spawnositions)
@@ -43,9 +44,8 @@ public class Criation_new_map : MonoBehaviour
             if (cc.a != 0)
             {
                 GameObject c = Instantiate(cube, pos, cube.transform.rotation);
-                 c.GetComponent<MeshRenderer>().material.renderQueue =  (int)UnityEngine.Rendering.RenderQueue.Transparent;
-                c.GetComponent<MeshRenderer>().material.color = new Color(cc.r,cc.g,cc.b,0.2f);
-                Debug.Log(c.GetComponent<Renderer>().sharedMaterial.color);
+                c.GetComponent<MeshRenderer>().material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                c.GetComponent<MeshRenderer>().material.color = new Color(cc.r, cc.g, cc.b, 0.2f);
                 GameObject child = c.transform.GetChild(0).gameObject;
                 child.GetComponent<TextMeshPro>().text = "2";
                 child = c.transform.GetChild(1).gameObject;
@@ -53,12 +53,6 @@ public class Criation_new_map : MonoBehaviour
             }
             counter++;
         }
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/scripts/FlipControlRight.cs~
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipControlRight : MonoBehaviour
+{
+    public static bool isKeyPress = false;
+    public static bool isTouched = false;
+
+
+    //#1
+    public float speed = 0f;
+    private HingeJoint myHingeJoint;
+    private JointMotor motor;
+
+
+    void Start()
+    {
+
+        // #2
+        myHingeJoint = GetComponent<HingeJoint>();
+        motor = myHingeJoint.motor;
+    }
+
+
+    void Update()
+    {
+
+
+        /////////////
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            isKeyPress = true;
+            Debug.Log("Right");
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            isKeyPress = false;
+            Debug.Log("Right");
+        }
+
+    }
+
+
+    void FixedUpdate()
+    {
+        // on press keyboard or touch Screen
+        if (isKeyPress == true && isTouched == false || isKeyPress == false && isTouched == true)
+        {
+
+            // #3
+            motor.targetVelocity = speed;
+            myHingeJoint.motor = motor;
+        }
+        else
+        {
+            // #4
+            motor.targetVelocity = -speed;
+            myHingeJoint.motor = motor;
+        }
+
+    }
+}
